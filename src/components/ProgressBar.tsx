23ea398ca9bfa909b5e67599adbbeb2a22c49b84
Shehzad Iqbal <shehzaddiqbal@gmail.com>
2023-09-05 19:26:42 +0500

    all code formatted with prettier

--- src/components/ProgressBar.tsx
+++ src/components/ProgressBar.tsx
@@ -2,7 +2,10 @@
 export default function ProgressBar({ percentage }: { percentage: number }) {
   return (
     <div className="w-full">
-      <div className="h-4 bg-blue-400" style={{ width: `${percentage}%` }}></div>
+      <div
+        className="h-4 bg-blue-400"
+        style={{ width: `${percentage}%` }}
+      ></div>
     </div>
   );
 }
--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -13,11 +13,11 @@ export default function Welcome({ setStart }: IProps) {
         src="/Expertizo-logo.png"
         alt="logo of Expertizo"
       />
-      <h1 className="mb-10 text-center break-words text-4xl font-bold lg:text-5xl">
+      <h1 className="mb-10 break-words text-center text-4xl font-bold lg:text-5xl">
         Welcome to Expertizo Quiz
       </h1>
       <button
-        className="rounded-md px-6 py-2 ring-2 ring-blue-400 duration-300 hover:transition-transform hover:bg-blue-300"
+        className="rounded-md px-6 py-2 ring-2 ring-blue-400 duration-300 hover:bg-blue-300 hover:transition-transform"
         onClick={() => setStart(true)}
       >
         Start Quiz
