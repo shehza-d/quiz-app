6f6262a75cd286a13dcebb2a8708e907bfa1c020
Shehzad Iqbal <shehzaddiqbal@gmail.com>
2023-09-08 12:55:35 +0500

    stars changed 5 to 3

--- src/components/Stars.tsx
+++ src/components/Stars.tsx
@@ -1,10 +1,11 @@
 export default function Stars({ filled }: { filled: number }) {
   return (
     <div className="flex gap-1">
-      {Array(5)
+      {Array(3)
         .fill(0)
         .map((item, i) => (
           <span
+            key={i}
             className={`relative text-2xl ${
               i < filled ? "text-black" : "text-[rgb(229,229,229)]"
             }`}
